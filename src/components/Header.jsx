bc19de6a29109e1d83aa4e6c0809a687ed0cc0c8
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-02-21 10:16:33 +0700

    feat: [home] add sidebar context and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { SidebarContext } from '../contexts/SidebarContext'
+import { BsBag } from 'react-icons/bs'
 
 export default function Header() {
+  const {setIsOpen, isOpen} = useContext(SidebarContext)
+
   return (
-    <div>Header</div>
+    <div>
+      <div>Header</div>
+      <div onClick={() => setIsOpen(!isOpen)} className='cursor-pointer flex relative'>
+        <BsBag className='text-2xl'/>
+      </div>
+    </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { IoMdArrowForward } from 'react-icons/io'
+import { FiTrash2 } from 'react-icons/fi'
+import CartItem from '../components/CartItem'
+import { SidebarContext } from '../contexts/SidebarContext'
 
 export default function Sidebar() {
+  const {isOpen, handleClose } = useContext(SidebarContext)
   return (
-    <div>Sidebar</div>
+    <div className={`${isOpen ? 'right-0' : '-right-full' } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
+      <div className='flex items-center justify-between py-6 border-b'>
+        <div className='uppercase text-sm font-semibold'>Shopping Bag (0)</div>
+        <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
+          <IoMdArrowForward className='text-2x;' />
+        </div>
+      </div>
+    </div>
   )
 }
