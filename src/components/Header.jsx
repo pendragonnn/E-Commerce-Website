5969ff09ac14389509ac5e7611f9a045631612c2
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-02-21 21:06:47 +0700

    feat: [cart] add shopping bag management feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { SidebarContext } from '../contexts/SidebarContext'
 import { BsBag } from 'react-icons/bs'
+import { CartContext } from '../contexts/CartContext'
+import { Link } from 'react-router-dom'
+import Logo from "../img/logo.svg"
 
 export default function Header() {
-  const {setIsOpen, isOpen} = useContext(SidebarContext)
+  const { setIsOpen, isOpen } = useContext(SidebarContext)
+  const { itemAmount } = useContext(CartContext)
+  const [isActive, setIsActive] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false)
+    })
+  })
 
   return (
-    <div>
-      <div>Header</div>
-      <div onClick={() => setIsOpen(!isOpen)} className='cursor-pointer flex relative'>
-        <BsBag className='text-2xl'/>
+    <header className={`${isActive? 'bg-white py-4 shadow-md' : 'bg-none py-6'} fixed w-full z-10 transition-all`}>
+      <div className='container mx-auto flex items-center justify-between h-full'>
+        <Link to={"/"}>
+          <div>
+            <img className='w-[40px]' src={Logo} alt="" />
+          </div>
+        </Link>
+        <div onClick={() => setIsOpen(!isOpen)} className='cursor-pointer flex relative'>
+          <BsBag className='text-2xl' />
+          <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center'>{itemAmount}</div>
+        </div>
       </div>
-    </div>
+    </header>
   )
 }
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,18 +1,38 @@
-import React, {createContext, useState, useEffect, Children } from 'react'
+import React, { createContext, useState, useEffect, Children } from 'react'
 
 export const CartContext = createContext()
 
 export default function CartProvider({ children }) {
   const [cart, setCart] = useState([])
+  const [itemAmount, setItemAmount] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    const total = cart.reduce((accumulator, currentItem) => {
+      return accumulator + currentItem.price * currentItem.amount
+    }, 0)
+    
+    setTotal(total)
+  })
+
+  useEffect(() => {
+    if(cart) {
+      const amount = cart.reduce((accumulator, currentIndex) => {
+        return accumulator + currentIndex.amount
+      }, 0)
+
+      setItemAmount(amount)
+    }
+  }, [cart])
 
   const addToCart = (product, id) => {
     console.log("Adding product:", product); // Cek struktur produk
     const newItem = { ...product, amount: 1 };
-  
+
     const cartitem = cart.find(item => item.id === id);
-  
+
     if (cartitem) {
-      const newCart = cart.map(item => 
+      const newCart = cart.map(item =>
         item.id === id ? { ...item, amount: cartitem.amount + 1 } : item
       );
       setCart(newCart);
@@ -20,12 +40,46 @@ export default function CartProvider({ children }) {
       setCart([...cart, newItem]);
     }
   };
-  
 
-  console.log(cart)
+  const removeFromCart = (id) => {
+    const newCart = cart.filter(item => {
+      return item.id !== id
+    })
+    setCart(newCart)
+  }
+
+  const clearCart = () => {
+    setCart([])
+  }
+
+  const increaseAmount = (id) => {
+    const cartItem = cart.find(item => item.id === id)
+    addToCart(cartItem, id)
+  }
+
+  const decreaseAmount = (id) => {
+    const cartItem = cart.find(item => {
+      return item.id === id
+    })
+
+    if (cartItem) {
+      const newCart = cart.map(item => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount - 1 }
+        } else {
+          return item
+        }
+      })
+      setCart(newCart)
+    }
+
+    if (cartItem.amount < 2) {
+      removeFromCart(id)
+    }
+  }
 
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, increaseAmount, decreaseAmount, itemAmount, total }}>
       {children}
     </CartContext.Provider>
   )
