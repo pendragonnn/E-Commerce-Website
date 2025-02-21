5b76d3a5b733c8def49500f4ac26db588dc24808
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-02-21 09:54:35 +0700

    feat: [home] display product in home page and link each of it with product id

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,12 +18,8 @@ export default function Home() {
       <section className='py-16'>
         <div className='container mx-auto'>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
-            {filteredProducts.map((product, id) => {
-              return (
-                <div className='w-full h-[300px] bg-pink-200 mb-4' key={id}>
-                  <Product />
-                </div>
-              )
+            {filteredProducts.map(product => {
+              return <Product key={product.id} product={product} />
             })}
           </div>
         </div>
